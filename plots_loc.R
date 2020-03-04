a6fa5b03dad3cd673834e6093f43957aab9bffe4
Alejandro Vaca Serrano <alejandrovaca@MacBook-Pro-de-Alejandro-3.local>
2020-03-05 00:38:05 +0100

    añadido plotly

--- plots_loc.R
+++ plots_loc.R
@@ -9,3 +9,5 @@ df = read.csv('Modelar_UH2020.txt', sep='|')
 p = ggplot(df, aes(x=X, y=Y, colour=CONTRUCTIONYEAR)) +
   geom_point(size=0.1) +
   scale_colour_continuous(type='viridis')
+
+ggplotly(p)
