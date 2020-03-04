e8efbb0628b2b53cde924e3c0f5a7dad26df9301
Alejandro Vaca Serrano <alejandrovaca@MacBook-Pro-de-Alejandro-3.local>
2020-03-05 00:35:24 +0100

    pruebas imbalanced-learn

---
+++ plots_loc.R
@@ -0,0 +1,11 @@
+library(ggplot2)
+library(data.table)
+install.packages('plotly')
+library(plotly)
+
+df = read.csv('Modelar_UH2020.txt', sep='|')
+
+
+p = ggplot(df, aes(x=X, y=Y, colour=CONTRUCTIONYEAR)) +
+  geom_point(size=0.1) +
+  scale_colour_continuous(type='viridis')
